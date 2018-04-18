dcd2632a10a7ef55aae28f654914822572198019
Thiago Werner <wernerthiago@gmail.com>
2018-04-18 01:00:35 -0300

    Update endpoint-pattern.ino

--- endpoint-pattern.ino
+++ endpoint-pattern.ino
@@ -5,9 +5,9 @@
 // LoRaWAN NwkSKey, network session key
 // This is the default Semtech key, which is used by the early prototype TTN
 // network.
-static const PROGMEM u1_t NWKSKEY[16] = { 0xf1, 0xd5, 0x26, 0xca, 0xab, 0x26, 0x2b, 0x66, 0x19, 0x0a, 0x3f, 0xc4, 0xf9, 0x64, 0x85, 0xf6 };
-static const u1_t PROGMEM APPSKEY[16] = { 0xf1, 0xd5, 0x26, 0xca, 0xab, 0x26, 0x2b, 0x66, 0x19, 0x0a, 0x3f, 0xc4, 0xf9, 0x64, 0x85, 0xf6 };
-static const u4_t DEVADDR = 0x07aa69a1 ; // <-- Change this address for every node!
+static const PROGMEM u1_t NWKSKEY[16] = {0xa5, 0xd6, 0x6b, 0xd7, 0x67, 0x76, 0xb7, 0x4e, 0x61, 0xd9, 0x89, 0x6e, 0x61, 0x6b, 0x9a, 0x81};
+static const u1_t PROGMEM APPSKEY[16] = {0xa5, 0xd6, 0x6b, 0xd7, 0x67, 0x76, 0xb7, 0x4e, 0x61, 0xd9, 0x89, 0x6e, 0x61, 0x6b, 0x9a, 0x81};
+static const u4_t DEVADDR = 0x06b6e7ab; // <-- Change this address for every node!
 
 // These callbacks are only used in over-the-air activation, so they are
 // left empty here (we cannot leave them out completely unless
@@ -16,19 +16,18 @@ void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-static uint8_t mydata[] = "Hello World";
 static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 5;
+const unsigned TX_INTERVAL = 15;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
   .nss = 10,
   .rxtx = LMIC_UNUSED_PIN,
   .rst = 8,
-  .dio = {2, 3, 4},
+  .dio = {2, 3, 4}
 };
 
 void onEvent (ev_t ev) {
@@ -101,29 +100,36 @@ void do_send(osjob_t* j) {
   if (LMIC.opmode & OP_TXRXPEND) {
     Serial.println(F("OP_TXRXPEND, not sending"));
   } else {
-    // Prepare upstream data transmission at the next possible time.
-    LMIC_setTxData2(1, mydata, sizeof(mydata) - 1, 0);
+    String aux = "ABIJCSUD";
+    byte binary[aux.length()];
+    for (int i = 0; i < aux.length(); i++)
+    {
+      binary[i] = byte(aux[i]);
+    }
+    //StringToBinary_End
+    LMIC_setTxData2(1, binary, sizeof(binary) - 1, 0);
     Serial.println(F("Packet queued"));
   }
   // Next TX is scheduled after TX_COMPLETE event.
 }
 
+
 void setup() {
   Serial.begin(115200);
   Serial.println(F("Starting"));
-  //GPS_Begin
-  ss.begin(9600);
-  //GPS_End
+
 #ifdef VCC_ENABLE
   // For Pinoccio Scout boards
   pinMode(VCC_ENABLE, OUTPUT);
   digitalWrite(VCC_ENABLE, HIGH);
   delay(1000);
 #endif
+
   // LMIC init
   os_init();
   // Reset the MAC state. Session and pending data transfers will be discarded.
   LMIC_reset();
+
   // Set static session parameters. Instead of dynamically establishing a session
   // by joining the network, precomputed session parameters are be provided.
 #ifdef PROGMEM
@@ -140,15 +146,23 @@ void setup() {
   LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
 #endif
   LMIC_selectSubBand(1);
+
   // Disable link check validation
   LMIC_setLinkCheckMode(0);
+
   // TTN uses SF9 for its RX2 window.
   LMIC.dn2Dr = DR_SF9;
+
   // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-  LMIC_setDrTxpow(DR_SF7, 14);
+  LMIC_setDrTxpow(DR_SF10, 14);
+
+  // Alterar taxa do código. Padrão 4/5.
+  LMIC.errcr = CR_4_8;
+
   // Start job
   do_send(&sendjob);
 }
+
 
 void loop() {
   os_runloop_once();
